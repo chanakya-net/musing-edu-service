d50646924e90e64980f172d37e618ca8961051ba
chanakya-1984 <43135504+chanakya-1984@users.noreply.github.com>
2020-03-31 12:47:17 +0530

    changed controllers

---
+++ musingDayCare/Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using musingDayCareCore.UserOprations.Command;
+using musingDayCareCore.UserOprations.Command.CreateNewUser;
+using musingDayCareCore.UserOprations.Query.ValidateUser;
+
+namespace musingDayCare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : BaseController
+    {
+
+
+
+        /// <summary>
+        /// Auth action Login User
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] CheckVaildUserQuery value)
+        {
+            var res = await Mediator.Send(value);
+            if (res != null)
+            {
+                var tokenData = await GetToken(res);
+                return Ok(tokenData);
+            }
+
+            return NotFound("Invalid Username or password");
+        }
+
+
+      
+
+        private async Task<dynamic> GetToken(UserInformationVM data)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, data.FirstName),
+                new Claim(ClaimTypes.NameIdentifier, data.UserName),
+                new Claim(JwtRegisteredClaimNames.Nbf, 
+                new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp, 
+                new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString())
+            };
+            foreach(var role in data.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            var token = new JwtSecurityToken(
+                new JwtHeader(
+                    new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisIsOnlyForTestingAndWillBeReplaced")), SecurityAlgorithms.HmacSha256)
+                    ), new JwtPayload(claims)
+                );
+            var tokenOutput = new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                firstname = data.FirstName,
+                lastname = data.LastName,
+                username = data.UserName
+            };
+            return tokenOutput;
+        }
+    }
+}
--- musingDayCare/Controllers/UserController.cs
+++ musingDayCare/Controllers/UserController.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using musingDayCareCore.UserOprations.Command;
 using musingDayCareCore.UserOprations.Command.CreateNewUser;
-using musingDayCareCore.UserOprations.Query.ValidateUser;
 
 namespace musingDayCare.Controllers
 {
@@ -19,63 +13,17 @@ namespace musingDayCare.Controllers
     [ApiController]
     public class UserController : BaseController
     {
-
-
-
-
-        [HttpPost]
-        [Route("Login")]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login([FromBody] CheckVaildUserQuery value)
-        {
-            var res = await Mediator.Send(value);
-            if (res != null)
-            {
-                var tokenData = await GetToken(res);
-                return Ok(tokenData);
-            }
-
-            return NotFound("Invalid Username or password");
-        }
-
-
+        /// <summary>
+        /// Creates new user
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("createuser")]
-
         public async Task<IActionResult> CreateUser([FromBody] CreateNewUserCommand data)
         {
             var res = await Mediator.Send(data);
             return Ok(res);
-        }
-
-        private async Task<dynamic> GetToken(UserInformationVM data)
-        {
-            var claims = new List<Claim>()
-            {
-                new Claim(ClaimTypes.Name, data.FirstName),
-                new Claim(ClaimTypes.NameIdentifier, data.UserName),
-                new Claim(JwtRegisteredClaimNames.Nbf, 
-                new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
-                new Claim(JwtRegisteredClaimNames.Exp, 
-                new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString())
-            };
-            foreach(var role in data.Roles)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, role));
-            }
-            var token = new JwtSecurityToken(
-                new JwtHeader(
-                    new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisIsOnlyForTestingAndWillBeReplaced")), SecurityAlgorithms.HmacSha256)
-                    ), new JwtPayload(claims)
-                );
-            var tokenOutput = new
-            {
-                token = new JwtSecurityTokenHandler().WriteToken(token),
-                firstname = data.FirstName,
-                lastname = data.LastName,
-                username = data.UserName
-            };
-            return tokenOutput;
         }
     }
 }
